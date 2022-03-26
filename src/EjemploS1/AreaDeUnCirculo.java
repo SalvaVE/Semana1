79dbda53d0157cbb70931894e08126f4a6f604f2
SalvaVE <101200743+SalvaVE@users.noreply.github.com>
2022-03-26 09:59:38 -0600

    Semana1

---
+++ src/EjemploS1/AreaDeUnCirculo.java
@@ -0,0 +1,9 @@
+
+package EjemploS1;
+
+public class AreaDeUnCirculo {
+    public static void main (String[] args){
+        
+    }
+ }
+
---
+++ src/EjemploS1/CalculoInteresSimple.java
@@ -0,0 +1,20 @@
+
+package EjemploS1;
+    
+
+public class CalculoInteresSimple {
+    public static void main (String[] args){
+        double interes, capitalPrestado, tiempo, tasadeinteres, tiempoenmeses;
+    
+    interes = 144;
+    capitalPrestado =  1200.00;
+    tasadeinteres = 0.08;
+    tiempo = interes / (capitalPrestado * tasadeinteres);
+    tiempoenmeses = tiempo * 12;
+    System.out.println("Tiempo: " + tiempo + "años");
+    System.out.println("Tiempo: " + tiempoenmeses + "meses");
+}
+    
+}
+    
+
